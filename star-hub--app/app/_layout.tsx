095a82222f59635f9d0436b55b71d0244d3495fb
matheus holanda <matheusdemoura2009@gmail.com>
2026-03-25 00:29:28 -0300

    Atualiza arquivo layout

--- star-hub--app/app/_layout.tsx
+++ star-hub--app/app/_layout.tsx
@@ -3,18 +3,9 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
-  );
-}
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary
